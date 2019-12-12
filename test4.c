7ed21f419864f758bcfcb1bc827795f8fca148df
Power Ranger <392138779@qq.com>
2019-12-13 04:17:37 +0800

    add

--- test4.c
+++ test4.c
@@ -173,8 +173,8 @@ void unsigned_int_operation(void)
     result = us1 - us2; //无符号数一般不这样用,这里只是验证一下无符号数的编码储存
     //本质是位向量的运算(bit vector),加上模和补数的知识,转换说明相当于"function",最后得到(映射到)结果,
     //映射到啥就是啥了~ 
-    //B2T[w](x) 二进制数 to(映射) 二补数(补码), 
-    //二进制数运算后的结果 映射 到的补数 = 真值(补码的巧妙之处,当然前提是补数和运算结果没有溢出)
+    //B2T[w](x:bit vector) {二进制数binary to 二补数(补码)two's complement} =映射--> 整数(真值)
+    //B2U[w](x:bit vector) {二进制数binary to 无符号数编码unsigned encodings} =映射--> 整数(真值)
     printf("Wrong result:%hu,Right result:%hd,Right result:%hu\n", result, result, us1 + us2);
     return;
 }
