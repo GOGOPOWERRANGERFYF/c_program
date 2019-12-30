1a6b28b3abab910b861d347871f5bea98cf7fdd6
Power Ranger <392138779@qq.com>
2019-12-31 02:52:45 +0800

    'commit'

--- test_base.c
+++ test_base.c
@@ -17,13 +17,11 @@
     |--> linker(ld is short for link editor)链接器
 */
 
-
 //program:程序,编码指令
 //这一段代码以字节序列的方式存储在文件中,每个字符都对应一个ascii码,中文字符是unicode码
 //只由ascii码构成的文件称为文本文件(也有unicode文本文件),所有其他文件都称为二进制文件
 //所有数据都是一串比特表示,区分不同数据对象的唯一方法是读取数据对象上下文,
 //在不同的上下文中,一个相同的字节序列可能表示一个整数,浮点数,字符串或机器指令.
-
 
 #include <stdio.h> \\standard input & output . head头文件
 //stdio.h是C编译器软件包的标准库的一个文件,它提供键盘输入和屏幕输出的支持
@@ -40,7 +38,6 @@
 //比如调用到printf函数,printf函数存在于一个名为printf.o的单独编译的可重定位目标程序中,
 //printf.o以某种方式合并到hello.o文件,ld负责处理这种合并,
 //合并后得到文件hello,可执行目标文件(简称可执行文件),可以加载到内存中,有系统执行.
-
 int main(void)
 {
     printf("hello world!\n");
