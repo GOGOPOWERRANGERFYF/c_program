25b52dc987005ba7de118e36c4eea0fd4b27813f
Power Ranger <392138779@qq.com>
2020-01-10 20:09:44 +0800

    'add'

--- test5.c
+++ test5.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+// Floating-Point Operations 浮点数运算
+// addition:
+//    
+//
 //exponent指数 指数计数法:1.0e9
 //             科学计数法:1.0*10^9
 //                   数字:1000000000
-int main(void)
+void main(void)
 {
-    float f1 = 111.111;
+    float f1 = 111.111, fa = 1.4, fb = 0.1;
+    // 1.4
 
     printf("%d\n",f1);
     printf("%d\n", sizeof(f1));
-    
-    return 0;
+    printf("%f\n", fa + fb);
+    printf("%f\n", fa - fb);
+    return;
 }
 
 
