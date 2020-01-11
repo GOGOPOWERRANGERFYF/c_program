2a0112938a47215c026e3fe3a390521bc2eb5c52
Power Ranger <392138779@qq.com>
2020-01-11 11:08:02 +0800

    'modify'

--- test5.c
+++ test5.c
@@ -11,12 +11,12 @@ void main(void)
     float f1 = 111.111, fa = 1.4, fb = 0.1;
     // 1.4 --> 1.
     // 0.1 --> 0.
-    printf("%d\n",f1);
-    printf("%d\n", sizeof(f1));
-    printf("%.10f, %.6a\n", fa, fa);
-    printf("%.10f, %.6a\n", fb, fb);
-    printf("%f, %a\n", fa + fb, fa + fb);
-    printf("%f, %a\n", fa - fb, fa - fb);
+    printf("f1(d):%d\n",f1);
+    printf("sizeof(f1):%d\n", sizeof(f1));
+    printf("fa:%.10f,fa:%.6a\n", fa, fa);
+    printf("fb:%.10f,fb:%.6a\n", fb, fb);
+    printf("fa + fb(f):%f,fa + fb(a): %a\n", fa + fb, fa + fb);
+    printf("fa - fb(f):%f,fa + fb(a): %a\n", fa - fb, fa - fb);
     return;
 }
 
