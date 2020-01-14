82c695832b55f3b4ed3e65e02e76ea1b925a8416
Power Ranger <392138779@qq.com>
2020-01-14 19:41:18 +0800

    'modify'

--- test5.c
+++ test5.c
@@ -19,9 +19,10 @@ void main(void)
     printf("fd(f):%f, fd(a):%a, fd(.7a):%.7a\n", fd, fd, fd);
     printf("fe(f):%f, fe(a):%a\n", fe, fe);
     //float double 格式说明符都是 %f
-    printf("dc(f):%f, dc(a):%a\n", fc, fc);
-    printf("dd(f):%f, dd(a):%a\n", fd, fd);
-    printf("de(f):%f, de(a):%a\n", fe, fe);
+    printf("dc(f):%f, dc(a):%a\n", dc, dc);
+    printf("dd(f):%f, dd(a):%a, dd(.7a):%.7a\n", dd, dd, dd);
+    printf("de(f):%f, de(a):%a\n", de, de);
+    
     printf("fa(.10f):%.10f,fa(a):%a\n", fa, fa);
     printf("fb(.10f):%.10f,fb(a):%a\n", fb, fb);
     printf("fa(.6a):%.6a\n", fa);
