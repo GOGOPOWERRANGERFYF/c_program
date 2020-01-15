93a708589475d14da35e41587cbb6f56f440b1d9
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-01-15 12:33:24 +0800

    modify

--- test5.c
+++ test5.c
@@ -127,6 +127,7 @@ void float_double_limits(void)
     float fa = 1.3999995;
     double da = 1.3999995;
     printf("fa(.52f):%.52f,\nda(.52f):%.52f\n", fa, da);
+    printf("fa(.53f):%.53f,\nda(.53f):%.53f\n", fa, da);
     return;
 }
 
