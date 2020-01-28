576e5fd24160be65318ed745d97d64006aea1b35
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-01-28 21:58:55 +0800

    add

--- test7.c
+++ test7.c
@@ -6,6 +6,7 @@ void short_int_array(void);
 void main(void)
 {
     //c_var[40]是一个可容纳40个字符的数组
+    //该数组占用内存中连续 1*40个字节byte
     char c_var[40];
     // \b 光标向左移一位,一个字符占一位 
     printf("Input You Name:__________\b\b\b\b\b\b\b\b\b\b");
@@ -21,6 +22,7 @@ void main(void)
 void short_int_array(void)
 {
     //短整型数组
+    //该数组占用内存中连续 2*5个字节byte
     short int si_array_a[5];
     printf("sizeof(si_array_a): %d", sizeof(si_array_a));
     return;
