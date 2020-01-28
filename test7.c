f7d4db4527a4672cf452bcdc3c3197de26e59da3
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-01-28 17:11:07 +0800

    modify

--- test7.c
+++ test7.c
@@ -1,2 +1,16 @@
 // ANSI(american national standards institute):美国国家标准学会
+#include <stdio.h>
+#include <string.h>
+
+void main()
+{
+    //c_var[40]是一个可容纳40个字符的数组
+    char c_var[40];
+    // \b 光标向左移一位,一个字符占一位 
+    printf("Input You Name:__________\b\b\b\b\b\b\b\b\b\b");
+    scanf("%s", c_var);
+    printf("My Name is: %s", c_var);
+    return;
+}
+
 
