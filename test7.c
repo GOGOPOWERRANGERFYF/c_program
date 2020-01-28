37c18f6930dbfdfa0c33f4e22005a589636f1a1b
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-01-28 22:44:34 +0800

    modify

--- test7.c
+++ test7.c
@@ -1,7 +1,6 @@
 // ANSI(american national standards institute):美国国家标准学会
 #include <stdio.h>
 #include <string.h>
-void short_int_array(void);
 
 void main(void)
 {
@@ -11,22 +10,9 @@ void main(void)
     // \b 光标向左移一位,一个字符占一位 
     printf("Input You Name:__________\b\b\b\b\b\b\b\b\b\b");
     scanf("%s", c_var);
-    printf("My Name is: %s\n", c_var);
-    // strlen()函数获取字符串的长度
+    printf("    My Name is:%s\n", c_var);
+    //strlen()函数获取字符串的长度
     printf("string length: %d\n", strlen(c_var));
-
-    short_int_array();
     return;
 }
-
-void short_int_array(void)
-{
-    //短整型数组
-    //该数组占用内存中连续 2*5个字节byte
-    short int si_array_a[5];
-    printf("sizeof(si_array_a): %d", sizeof(si_array_a));
-    return;
-}
-
-
 
