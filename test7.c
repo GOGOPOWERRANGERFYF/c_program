c317d8898a55d768a6bbd625d545bdb35fbb50bc
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-01-31 21:44:57 +0800

    modify

--- test7.c
+++ test7.c
@@ -81,9 +81,9 @@ void circle_float_arithmetic(void)
     scanf("%f", &r);
     l = 2.0 * PI * r;
     a = PI * r * r;
-    printf("circle_l: %.2f, circle_a: %.2f\n", l, a);
-    printf("circle_l: %e, circle_a: %e\n", l, a);
-    printf("circle_l: %a, circle_a: %a\n", l, a);
+    printf("circle_l: %f, circle_a: %f\n", l, a);
+    //printf("circle_l: %e, circle_a: %e\n", l, a);
+    //printf("circle_l: %a, circle_a: %a\n", l, a);
     return;
 }
 
@@ -92,13 +92,13 @@ void circle_double_arithmetic(void)
     double d_r, d_l, d_a;
     printf("sizeof(d_r): %zd,sizeof(PI): %zd\n",
      sizeof(d_r), sizeof(PI));
-    printf("input r(floating-point number):______\b\b\b\b\b\b");
+    printf("input d_r(floating-point number):______\b\b\b\b\b\b");
     scanf("%lf", &d_r);
     d_l = 2.0 * PI * d_r;
     d_a = PI * d_r * d_r;
-    printf("circle_l: %.2f, circle_a: %.2f\n", d_l, d_a);
-    printf("circle_l: %e, circle_a: %e\n", d_l, d_a);
-    printf("circle_l: %a, circle_a: %a\n", d_l, d_a);
+    printf("circle_l: %.15f, circle_a: %.15f\n", d_l, d_a);
+    //printf("circle_l: %e, circle_a: %e\n", d_l, d_a);
+    //printf("circle_l: %a, circle_a: %a\n", d_l, d_a);
     return;
 }
 
