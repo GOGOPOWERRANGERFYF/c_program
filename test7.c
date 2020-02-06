4c0fb9077ac274fc1cb924bee6b2c5ae87e8c99e
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-02-06 14:42:52 +0800

    modify

--- test7.c
+++ test7.c
@@ -237,8 +237,12 @@ void conversion_specification_modifiers_and_flags(void)
 {
     const double cd_a = 123456.789;
     printf("%.3f\n", cd_a);
+    // %E e输出变大写字母E
+    printf("%E\n", cd_a);
     // flags # 显示前缀
     printf("%#lx\n", cd_a);
+    // %X 十六进制中的字母输出大写字母格式
+    printf("%#lX\n", cd_a);
     // .3d 整数数值位数不够的前面填充0 
     // 5d  整数数值位数不够的前面填充空格
     // 相同点,整数数值位数足够或超过的,以原值为准输出
