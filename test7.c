540f0e584617519f6a81220e5ccbc7d1161ff218
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-02-09 13:19:15 +0800

    modify

--- test7.c
+++ test7.c
@@ -300,12 +300,12 @@ void printf_function_stack(void)
     //有符号整数常量4byte, 32bit编码储存不了有符号数3220176896,编译器转换成64bit储存
     printf("int constant 3220176896(%%x): %x, sizeof(3220176896): %zd\n",
      3220176896, sizeof(3220176896));
+    printf("int constant 3220176896U(%%x): %x, sizeof(3220176896U): %zd\n",
+     3220176896U, sizeof(3220176896U));
     //为正数时前面的0输出是被省略了,输出负数时就可以看出前面是有填充1的
     printf("int constant 3220176896(%%lx): %lx\n", 3220176896);
     printf("int constant -3220176896(%%lx): %lx\n", -3220176896);
     printf("int constant -3220176896(%%x): %x\n", -3220176896);
-    printf("int constant 3220176896U(%%x): %x, sizeof(3220176896U): %zd\n",
-     3220176896U, sizeof(3220176896U));
     printf("3220176896U(%%lx): %lx\n", 3220176896U);
     printf("%d\n", usi_a);
     printf("%f\n", usi_a);
