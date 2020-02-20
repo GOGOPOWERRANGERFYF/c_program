043b84f4421a938d4bf050d6488ddc7ff646c93e
Power Ranger <392138779@qq.com>
2020-02-20 23:24:15 +0800

    modify

--- test8.c
+++ test8.c
@@ -134,6 +134,8 @@ void scanf_conversion_specification_character(void)
     scanf(" %c", &c_b);
     printf("c_b(%%c):%c\n", c_b);
     // 'A' ascii码是65
+    // %hhu printf和scanf都有这个转换说明
+    // 作用: 输出或输入一个一个字节的无符号整数
     printf("c_b(%%hhu):%hhu\n", c_b);
     return;
 }
