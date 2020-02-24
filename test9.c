a5e96c20168f82a7f8e26f04c05c1740cd41f27b
Power Ranger <392138779@qq.com>
2020-02-24 13:29:38 +0800

    modify

--- test9.c
+++ test9.c
@@ -15,8 +15,10 @@ int main(void)
 void while_statements(void)
 {
     int start = 0;
+
     while(start<10)             /* starting the while loop  */
-    {                           /* start of block */
+    //花括号之间的内容是要被重复的内容,用计算机术语说,就是程序循环这些语句
+    {                           /* start of block,花括号以及花括号括起来的部分被称为block*/
         start = start + 1;
         printf("%d\n", start);
     }                           /* end of block */
