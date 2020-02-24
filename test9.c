4dcc2240be2da76950b13081d1199dce058b5562
Power Ranger <392138779@qq.com>
2020-02-24 20:13:28 +0800

    modify

--- test9.c
+++ test9.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 //定义符号常量,预编译时后面的字符串替换前面的符号常量(文本)
 #define OPERATORS "= + - * /"
-void while_statements(void);
+void while_block(void);
 
 int main(void)
 {
@@ -9,11 +9,11 @@ int main(void)
     // = 赋值运算符
     //格式字符串中的转换说明%s,后面的待输出项类型和熟练要与转换说明相匹配
     printf("These is operators: %s\n", OPERATORS);
-    while_statements();
+    while_block();
     return 0;
 }
 
-void while_statements(void)
+void while_block(void)
 {
     //把整数值0赋给变量start,赋值表达式
     int start = 0;
