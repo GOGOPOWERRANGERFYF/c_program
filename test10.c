ef08bcb6e1e62cf918912d3cf9fb30126dc27632
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-02-29 14:59:37 +0800

    modify

--- test10.c
+++ test10.c
@@ -6,6 +6,9 @@ void main(void)
     // ++ 递增运算符(increment operator),将其运算对象operand递增1
     // 看了运行结果,i++后缀模式,表达式本身没自增
     //             ++i前缀模式,表达式本身有自增
+    //test_base.c 重要知识点(编译系统的编译过程)
+    //gcc --help查看gcc帮助文档
+    //通过汇编文件(文本格式)理解
     printf("%d %d\n", i_a, i_a++);
     printf("%d %d\n", i_b, ++i_b);
     return;
