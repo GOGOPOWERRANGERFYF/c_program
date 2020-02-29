4dc9cfab379317f642a44d4cd16e665679239f54
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-02-29 14:17:09 +0800

    modify

--- test_base.c
+++ test_base.c
@@ -39,20 +39,21 @@
 //操作系统实现这种进程间交错执行的机制称为上下文切换
 
 #include <stdio.h> /* standard input & output . head头文件 */
-//stdio.h是C编译器软件包的标准库的一个文件,它提供键盘输入和屏幕输出的支持
-//cpp(c pre-processor:预处理器)会读取stdio.h文件的内容并插入到程序文本中
-//结果得到另外一个C程序,通常以.i作为文件扩展名
-//源程序(文本)hello.c --[cpp]-->修改了的源程序(文本)hello.i,
-//旁注:(这里的文本编码格式是utf-8, 8bit unicode transformation format);
-//     utf-8可节省编码储存长度,用utf-8编码unicdoe长度可变;
-//     unicode码(4字节)是兼容ascii码(1字节)的.
-//hello.i --[ccl: c compiler]--> hello.s汇编程序(文本)
-//hello.s --[as: assembler]--> hello.o(relocatable object program可重定位目标程序(二进制文件))
-//hello.o --[ld: linker]--> hello可执行目标程序(二进制)
-//把hello程序要调用到的标准C库的函数合并到hello.o程序中,
-//比如调用到printf函数,printf函数存在于一个名为printf.o的单独编译的可重定位目标程序中,
-//printf.o以某种方式合并到hello.o文件,ld负责处理这种合并,
-//合并后得到文件hello,可执行目标文件(简称可执行文件),可以加载到内存中,有系统执行.
+// * 重要知识点(编译系统的编译过程):
+// * stdio.h是C编译器软件包的标准库的一个文件,它提供键盘输入和屏幕输出的支持
+// * cpp(c pre-processor:预处理器)会读取stdio.h文件的内容并插入到程序文本中
+// * 结果得到另外一个C程序,通常以.i作为文件扩展名
+// * 源程序(文本)hello.c --[cpp]-->修改了的源程序(文本)hello.i,
+// * 旁注:(这里的文本编码格式是utf-8, 8bit unicode transformation format);
+// *      utf-8可节省编码储存长度,用utf-8编码unicdoe长度可变;
+// *      unicode码(4字节)是兼容ascii码(1字节)的.
+// * hello.i --[ccl: c compiler]--> hello.s汇编程序(文本)
+// * hello.s --[as: assembler]--> hello.o(relocatable object program可重定位目标程序(二进制文件))
+// * hello.o --[ld: linker]--> hello可执行目标程序(二进制)
+// * 把hello程序要调用到的标准C库的函数合并到hello.o程序中,
+// * 比如调用到printf函数,printf函数存在于一个名为printf.o的单独编译的可重定位目标程序中,
+// * printf.o以某种方式合并到hello.o文件,ld负责处理这种合并,
+// * 合并后得到文件hello,可执行目标文件(简称可执行文件),可以加载到内存中,有系统执行.
 int main(void)
 {
     float f_var_a;
