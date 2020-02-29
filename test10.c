ca1801d311ea09928796db38afd216fd50a16dd1
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-02-29 12:53:48 +0800

    modify

--- test10.c
+++ test10.c
@@ -3,8 +3,9 @@
 void main(void)
 {
     int i_a = 1, i_b = 1;
+    // ++ 递增运算符(increment operator),将其运算对象operand递增1
     // 看了运行结果,i++后缀模式,表达式本身没自增
-    //             ++i前缀模式,表达式本身自增
+    //             ++i前缀模式,表达式本身有自增
     printf("%d %d\n", i_a, i_a++);
     printf("%d %d\n", i_b, ++i_b);
     return;
