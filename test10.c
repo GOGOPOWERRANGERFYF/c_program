e26d4c636f672efed96601be205bff3b5a6ee2d4
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-02-29 12:48:53 +0800

    modify

--- test10.c
+++ test10.c
@@ -4,7 +4,7 @@ void main(void)
 {
     int i_a = 1, i_b = 1;
     // 看了运行结果,i++后缀模式,表达式本身没自增
-    //             ++i前缀模式,变量值本身自增
+    //             ++i前缀模式,表达式本身自增
     printf("%d %d\n", i_a, i_a++);
     printf("%d %d\n", i_b, ++i_b);
     return;
