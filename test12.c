87505efd698d724ad2a67ed519d04c1edccb77cd
Power Ranger <392138779@qq.com>
2020-03-11 17:58:37 +0800

    modify

--- test12.c
+++ test12.c
@@ -39,9 +39,14 @@ void part_one_type_conversion(void)
     //<深入理解计算机系统第三版>P79 非规格化的值: mantissa尾数是不隐含1的
     //当exponent阶码全为0时,表示的是浮点数的非规格化形式
     // B2U binary当做unsigned --mapper映射--> 真值true value
-    printf("%a\n", 9223372036854775808LU);
-    printf("%e\n", 9223372036854775808LU);
-    printf("%f\n", 9223372036854775808LU);
+    // printf()会把float参数转换成double,因此 %f和%lf在printf函数是一样的,在scanf函数才会区分
+    printf("9223372036854775808LU %%f: %f\n", 9223372036854775808LU);
+    printf("9223372036854775808LU %%e: %e\n", 9223372036854775808LU);
+    printf("9223372036854775808LU %%a: %a\n", 9223372036854775808LU);
+    printf("---------------- size: 16 character\n");
+    printf("%16lx (%%16lx):2251799813685248LU\n", 2251799813685248LU);
+    printf("2251799813685248LU %%f: %f\n", 2251799813685248LU);
+    printf("2251799813685248LU %%a: %a\n", 2251799813685248LU);
     printf("%u\n", 2147483648U);
     printf("%d\n", 2147483648U);
     return;
