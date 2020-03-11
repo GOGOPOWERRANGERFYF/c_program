b206161cc6a4a25a276c7d397af665e8018694f3
Power Ranger <392138779@qq.com>
2020-03-11 14:52:38 +0800

    modify

--- test12.c
+++ test12.c
@@ -4,11 +4,17 @@ void main(void)
 {
     short int a = 1;
     unsigned short int b = 1;
-    printf("%zd\n", sizeof(a));
+
+    printf("sizeof(a) %%zd: %zd\n", sizeof(a));
     //短整型变量a,出现在表达式(expression,表达式由运算符和运算对象组成)会被自动转换成int
-    printf("%zd\n", sizeof(-a));
-    printf("%zd\n", sizeof(a+1));
+    printf("sizeof(-a) %%zd: %zd\n", sizeof(-a));
+    printf("sizeof(a+1) %%zd: %zd\n", sizeof(a+1));
+    
+    printf("sizeof(b) %%zd: %zd\n", sizeof(b));
+    printf("sizeof(-b) %%zd: %zd\n", sizeof(-b));
+    printf("sizeof(b+1) %%zd: %zd\n", sizeof(b+1));
     return;
 }
+
 
 
