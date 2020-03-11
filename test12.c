62cecabe5a28a7d1400af2b07b600e2d3c7b5435
Power Ranger <392138779@qq.com>
2020-03-11 21:27:56 +0800

    modify

--- test12.c
+++ test12.c
@@ -67,14 +67,17 @@ void assignment_statement_type_conversion(void)
 {
     short int astc_a, astc_x = 10;
     int astc_b = 2147483647, astc_y;
+
     //在赋值表达式中,计算结果会转换成 被赋值变量 的类型
-    //可能会降级(demotion),也可能会升级(promotion)
-    printf("%zd\n", sizeof(astc_a = astc_b));
-    astc_a = astc_b;
-    printf("%hd\n", astc_b);
+    //赋值表达式计算结果降级(demotion)的情况
+    printf("sizeof(astc_b) %%zd : %zd\n", sizeof(astc_b));
+    printf("sizeof(astc_a) %%zd : %zd\n", sizeof(astc_a));
+    printf("sizeof(astc_a = astc_b) %%zd :%zd\n", sizeof(astc_a = astc_b));
 
     //赋值表达式计算结果升级(promotion)的情况
-    printf("%zd\n", sizeof(astc_y = astc_x));
+    printf("sizeof(astc_x) %%zd : %zd\n", sizeof(astc_x));
+    printf("sizeof(astc_y) %%zd : %zd\n", sizeof(astc_y));
+    printf("sizeof(astc_y = astc_x) %%zd :%zd\n", sizeof(astc_y = astc_x));
     return;
 }
 
