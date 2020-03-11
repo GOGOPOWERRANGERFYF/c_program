b6affcdebf4a630fa73bd868e0eab7408ac2d063
Power Ranger <392138779@qq.com>
2020-03-11 14:41:42 +0800

    modify

--- test12.c
+++ test12.c
@@ -3,6 +3,9 @@
 void main(void)
 {
     short int a = 1;
+    unsigned short int b = 1;
+    printf("%zd\n", sizeof(a));
+    //短整型变量a,出现在表达式(expression,表达式由运算符合运算对象组成)会被自动转换成int
     printf("%zd\n", sizeof(a+1));
     return;
 }
