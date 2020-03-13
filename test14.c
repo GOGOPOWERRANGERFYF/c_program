5ef041eb36921e6f3e7bfe13811d41fe4d59ccd0
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-03-13 22:41:31 +0800

    modify

--- test14.c
+++ test14.c
@@ -3,6 +3,12 @@
 //scanf()函数返回值,读取成功返回1,读取失败返回0
 void main(void)
 {
+    long int num;
+    unsigned short int status;
+    
+    status = scanf("%ld", &num);
+    printf("%hu\n", status);
     return;
 }
+
 
