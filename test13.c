b83f86150b1464ee4af7a209ae076e90b401c8ca
Power Ranger <392138779@qq.com>
2020-03-13 15:14:37 +0800

    modify

--- test13.c
+++ test13.c
@@ -1,1 +1,29 @@
 // 带参数的函数
+#include <stdio.h>
+
+void while_loop_function(int n);
+
+void main(void)
+{
+    char c;
+    int a;
+    float f;
+    //自动类型转换
+    c = 'A';
+    a = 5.5F;
+    f = 10;
+    while_loop_function(c);
+    while_loop_function(a);
+    while_loop_function(f);
+    return;
+}
+
+void while_loop_function(int n)
+{
+    while (n-- > 0){
+        printf("-");
+    }
+    printf("\n");
+    return;
+}
+
