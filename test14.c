fdbde33bd1922bb843488c22552b62380f891c17
FYF <392138779@qq.com>
2020-03-17 16:56:41 +0800

    modify

--- test14.c
+++ test14.c
@@ -3,16 +3,19 @@
 //scanf()函数返回值,读取成功返回1,读取失败返回0
 void main(void)
 {
-    long int num;
+    long int num, sum = 0L;
     unsigned short int status;
     
+    printf("input integer:");
     status = scanf("%ld", &num);
-    printf("%hu\n", status);
+    printf("status: %hu\n", status);
+    while (status == 1){
+        printf("sum: %ld\n", num);
+        sum = num + sum;
+        printf("input integer:");
+        scanf("%ld", &num);
+    }
+    printf("Game Over!");
     return;
 }
-
-
-
-
-
 
