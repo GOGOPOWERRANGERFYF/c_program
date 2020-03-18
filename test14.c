faba91397b6e4f0ae7d232e147e18d9b03b17ff6
FYF <392138779@qq.com>
2020-03-18 17:02:30 +0800

    modify

--- test14.c
+++ test14.c
@@ -2,6 +2,9 @@
 //scanf()函数返回值,读取成功返回1,读取失败返回0
 void main(void)
 {
+    //这里用long int类型常量(constant)0L,
+    //用0其实也没问题(int类型常量0会隐式转换为long int)
+    //但还是建议保持数据类型的一致性
     long int num, sum = 0L;
     unsigned short int status;
     
