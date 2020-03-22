36f3b41aa4bf5b03fe9e1324a5c3dac76d636985
FYF <392138779@qq.com>
2020-03-22 22:10:02 +0800

    modify

--- test16.c
+++ test16.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 // C99新增_Bool类型   
 // 表示真或假的变量称为布尔变量(boolean variable)
+// _Bool类型变量只能储存1(真)或0(假)
+// 把非零值赋给_Bool类型变量是,该变量会被设置为1(这反映C把所有非零值视为真)
 int main(void)
 {
     return 0;
 }
+
+
+
+
+
