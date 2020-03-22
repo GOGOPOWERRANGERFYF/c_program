10d0b5f2f18386d570be8d2409743e52b4177e40
FYF <392138779@qq.com>
2020-03-22 22:28:12 +0800

    modify

--- test16.c
+++ test16.c
@@ -5,6 +5,11 @@
 // 把非零值赋给_Bool类型变量是,该变量会被设置为1(这反映C把所有非零值视为真)
 int main(void)
 {
+    _Bool a = 1, b = 'a', c = 0, d = 88;
+    printf("a = 1: %3d\n", a);
+    printf("b = 'a': %d\n", b);
+    printf("c = 0: %3d\n", c);
+    printf("d = 88: %2d\n", d);
     return 0;
 }
 
