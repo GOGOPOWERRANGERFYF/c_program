883228f7ade8248392b6ae4dc3211908ed4939ba
FYF <392138779@qq.com>
2020-03-22 14:49:31 +0800

    modify

--- test15.c
+++ test15.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(void)
 {
+    //while循环经常依赖测试表达式作比较,
+    //这样的表达式被称为关系表达式(relational expression)
+    //出现在关系表达式中的运算符被称为关系运算符(relational operator)
     return;
 }
+
 
 
