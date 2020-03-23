109d4bf7721d1d311b219eb23c75d5241fb361e3
FYF <392138779@qq.com>
2020-03-23 16:54:37 +0800

    modify

--- test16.c
+++ test16.c
@@ -27,7 +27,7 @@ int bool_variable(void)
     // 包含了该头文件后,代码可以与C++兼容,true和false,bool为c++的关键字
     bool a;
     int b;
-    //
+    printf("input decimal number:\n");
     // 1. 精简写法:
     while (scanf("%d", &b)){
         printf("true\n");
