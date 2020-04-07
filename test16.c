5c8039e5cfaca0860cc20d7b5ced8153731b3153
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-04-07 22:34:25 +0800

    modify

--- test16.c
+++ test16.c
@@ -52,7 +52,7 @@ int main(void)
     // 输入 10 文本(ascii码),编译系统把它转换成 10.0 文本,再转换成double类型的binary储存
     // (编译器具体怎么实现不清楚,但这个思路和结果应该是对的...
     //  至少结果是对的,目前水平有限,这个思路让我能理解,未必精准或正确)
-    // 输入 a 文本或符号文本,则输入错误,
+    // 输入 a 文本或符号文本(这些转换成表示double类型的文本想想就过分啊...),所以当然输入错误咯...
     while (scanf(" %lf %d", &input_n, &input_limit_e) == 2){
         power_return_value = power(input_n, input_limit_e);
         printf("number^exponent = %.2f\n", power_return_value);
