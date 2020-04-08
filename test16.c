0c0e49b09b671dcd99893bf0f89846cd07169a32
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-04-08 17:09:30 +0800

    modify

--- test16.c
+++ test16.c
@@ -27,7 +27,7 @@ double power(double n, int limit_e);
 //2.printf(),scanf()这些函数的原型声明(function prototype declaration)包含在stdio.h头文件里,
 //  #include <stdio.h>预处理指令读取并插入头文件里的文本 
 //  附:函数定义(function definition)在其它文件
-//测试
+
 // 这段main()程序是一个驱动(driver)示例,这段程序用来测试其它函数(function)
 int main(void)
 {
