6b36adc0f368777e4dca425527d3d465acce09d1
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-04-10 21:29:48 +0800

    modify

--- test17.c
+++ test17.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
+
 int main(void)
 {
+    const double pass_line = 60.0;
+    double score;
+    int pass_count = 0, input_count = 0;
+
+    pintf("Input score:\n");
+    while (scanf("%lf", &score) == 1){
+        input_count++;
+        if (score >= pass_line){
+            pass_count++;
+        }
+    }
+
+    if (input_count != 0){
+        printf("How many scores: %d, ");
+        printf("How many pass: %d");
+    }
+    if (input_count == 0){
+        printf("No score!");
+    }
+
     return 0;
 }
 
