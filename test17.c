5eb59cdedb4ddb1636f7309e695c7d617c3506a6
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-04-11 02:49:18 +0800

    modify

--- test17.c
+++ test17.c
@@ -15,14 +15,21 @@ int main(void)
         }
     }
 
+    //if (input_count != 0){
+    //    printf("How many scores: %d, ", input_count);
+    //    printf("How many pass: %d", pass_count);
+    //}
+    //if (input_count == 0){
+    //    printf("No score!");
+    //}
+
     if (input_count != 0){
         printf("How many scores: %d, ", input_count);
         printf("How many pass: %d", pass_count);
     }
-    if (input_count == 0){
+    else {
         printf("No score!");
     }
-
     return 0;
 }
 
