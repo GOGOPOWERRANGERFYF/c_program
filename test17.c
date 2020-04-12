1387dc9e25f66553da42fb1bcb1d32f9fb9393e8
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-04-12 15:38:25 +0800

    modify

--- test17.c
+++ test17.c
@@ -68,6 +68,7 @@ int getchar_putchar(void)
     ch = getchar();
     putchar(ch);
     printf("enter key(ASCII): %d;", ch);
+    // escape character 转义字符
     /* '\' 续行符和转移字字符; '\\' 把\转义为符号字符 */
     printf("\\n(ASCII): %d.", '\n');
     // short和character会被自动转换(隐式转换)为int,升级(promotion)
