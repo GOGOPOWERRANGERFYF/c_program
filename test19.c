ff2330cbacb143441d72fad81c5eb882e68d4b7c
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-04-20 17:05:52 +0800

    modify

--- test19.c
+++ test19.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#define SPACE ' '
+#define COMMA ','
+int char_count(void);
 
 int main(void)
 {
+    char_count();
     return 0;
 }
 
+int char_count(void)
+{
+    char ch;
+    unsigned int count = 0;
+
+    while ((ch = getchar()) != '.'){
+        if (ch != SPACE && ch != COMMA){
+            count++;
+        }
+    }
+
+    printf("character count: %d\n", count);
+    return 0;
+}
+
+// 逻辑运算符
+// && 与
+// || 或
+// !  非
