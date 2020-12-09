f4ae548bb4b47624a14bf810ed244bae87308988
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-12-09 16:36:05 +0800

    modify

--- algorithm1.c
+++ algorithm1.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+
+int algorithm_1(void);
+
 // algorithm 算法
-int main()
+int main(void)
 {
     printf("algorithm!\n");
+    algorithm_1();
     return 0;
 }
 
-
+int algorithm_1(void)
+{
+    return 0;
+}
 
