ed7782158424b80e27824c47fd9479b2ee7a7347
GOGOPOWERRANGERFYF <392138779@qq.com>
2020-12-25 20:37:17 +0800

    moidfy

--- algorithm1.c
+++ algorithm1.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
-int quick_find_algorithem(void);
+int quick_find_algorithem_version1(void);
 
 // algorithm 算法
 int main(void)
 {
     printf("algorithm!\n");
-    quick_find_algorithem();
+    quick_find_algorithem_version1();
     return 0;
 }
 
 // 数组(array)是基本的数据结构
 // 《算法:C语言实现》P6 程序1.1: p:数组的元素的下标 q:数组的元素的下标
-int quick_find_algorithem(void)
+int quick_find_algorithem_version1(void)
 {
     return 0;
 }
