da2e4fa8e51d3933180d4dbb2b7f2639c39aa9a8
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-01-11 23:08:32 +0800

    modify

--- algorithm2.c
+++ algorithm2.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
+typedef float height;
+void main(void){
+    height h1 = 1.72;
+    printf("person height: %.2fm\n", h1);
+    return;
+}
 // 《算法:C语言实现》 
 //  第二部分 数据结构
 //  abstract data type 抽象数据类型
+//  int float char 这些为c语言内置数据类型
 //  定义3.1 数据类型是值的集合和在这些值上的操作集  
 //  declare 声明
