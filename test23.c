d1a803af1f771bbf940b74d07f3d536def320697
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-01-13 03:41:26 +0800

    modify

--- test23.c
+++ test23.c
@@ -9,6 +9,11 @@ void main(void){
 }
 
 void array_a(void){
-    int array1[12];
+    // 只存储单个值的变量有时也称标量变量(scalar variable)
+    int i1 = 1;
+    // 从ANSI C开始支持这种初始化,
+    // 不支持ANSI的编译器需要在数组声明前加static
+    // 逗号分隔的值列表
+    int array1[5] = {2,4,6,8,10};
     return;
 }
