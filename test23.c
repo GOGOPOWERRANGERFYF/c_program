0fb13e0bd45aed1a5de6c9705609e9821f7bfea3
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-01-13 21:47:48 +0800

    modify

--- test23.c
+++ test23.c
@@ -39,5 +39,7 @@ void array_a(void){
     printf("array2[0]: %d\n", array2[0]);
     printf("array3[0]: %d\n", array3[3]);
     printf("array_auto1[0]: %d\n", array_auto1[7]);
+    // size 大小,尺码
+    printf("array size of array2: %d\n", sizeof(array2));
     return;
 }
