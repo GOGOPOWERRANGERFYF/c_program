5570305c39ba97978a6ab85c4b074d8ffe1daec8
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-01-13 21:13:40 +0800

    modify

--- test23.c
+++ test23.c
@@ -11,12 +11,16 @@ void main(void){
 void array_a(void){
     // 只存储单个值的变量有时也称标量变量(scalar variable)
     int i1 = 1;
+
+    const int birthday[3] = {1987,11,9};
     // 从ANSI C开始支持这种初始化,
     // 不支持ANSI的编译器需要在数组声明前加static
     // 逗号分隔的值列表
     int array1[5] = {2,4,6,8,10};
     int array2[5];
-    const int birthday[3] = {1987,11,9};
+    // 初始化列表的值的个数少于数组元素个数时,编译器会把剩余的元素初始化为0
+    int array3[5] = {2,4};
+
     // storage class存储类别
     // 数组与其他变量类似,可以把数组创建成不同存储类别
     // 第十二章将介绍存储类别的相关内容
@@ -29,7 +33,7 @@ void array_a(void){
     // 对于其他一些存储类别的变量和数组,如果声明时未初始化
     // 编译器会把它们的值设置为0
     printf("%d\n", array2[0]);
+    printf("%d\n", array3[3]);
 
-    //
     return;
 }
