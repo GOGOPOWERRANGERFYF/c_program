10aacfe124c90358f49beb8261955fb5e45e13a2
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-01-20 18:44:22 +0800

    modify

--- test25.c
+++ test25.c
@@ -11,10 +11,20 @@ void main(void){
 // malloc 全称:memory allocation
 // allocation 分配,拨给的场地
 void memory_allocation(void){
+    char * char_pointer;
     int * int_pointer;
-    int_pointer = malloc(4 * sizeof(int));
+    int_pointer = (int *)malloc(4 * sizeof(int));
+    *int_pointer = 1;
+    *(int_pointer + 1) = 2;
+    *(int_pointer + 2) = 3;
+    *(int_pointer + 3) = 4;
+
     printf("%zd\n", sizeof(int));
     printf("%d\n", 4 * sizeof(int));
-    //printf("%p\n", int_pointer);
+
+    printf("%d\n", *int_pointer);
+    printf("%d\n", *(int_pointer + 1));
+    printf("%d\n", *(int_pointer + 2));
+    printf("%d\n", *(int_pointer + 3));
     return;
 }
