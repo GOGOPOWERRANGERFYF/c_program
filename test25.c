db1824a72d3d837752749c30426b4dc1df448097
GOGOPOWERRANGERFYF <392138779@qq.com>
2021-01-20 17:16:58 +0800

    add

--- test25.c
+++ test25.c
@@ -1,8 +1,18 @@
 // 第12章 存储类别.链接和内存管理
 #include <stdio.h>
+#include <stdlib.h>
+void memory_allocation(void);
+
 void main(void){
+    memory_allocation();
     return;
 }
 
 // malloc 全称:memory allocation
 // allocation 分配,拨给的场地
+void memory_allocation(void){
+    int * int_pointer;
+    int_pointer = malloc(8);
+    printf("%p\n", int_pointer);
+    return;
+}
